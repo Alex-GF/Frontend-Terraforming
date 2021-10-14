b1deb440aaff8267925cc1b81da78213d70ccf05
Alex-GF <alegarfer4@alum.us.es>
2021-10-14 19:53:42 +0200

    Ultima actu 14 Octubre

---
+++ src/components/profile/winRate.js
@@ -0,0 +1,26 @@
+const WinRate = () => {
+
+    return(
+
+        <>
+            <div className="row">
+                <div className="col-6">
+                    <p className="stat-name">Partidas Jugadas</p>
+                    <p className="stat">69</p>
+                </div>
+                <div className="col-6">
+                    <p className="stat-name">Partidas Ganadas</p>
+                    <p className="stat">31</p>
+                </div>
+            </div>
+            <div className="row">
+                <div id="star">
+                    
+                </div>
+            </div>
+        </>
+    );
+
+}
+
+export default WinRate;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,6 +2,7 @@ import FotoUser from "../components/profile/fotoUser";
 import GlobalPunc from "../components/profile/globalPunc";
 import Achievements from "../components/profile/achievements";
 import FriendRequest from "../components/profile/friendRequest";
+import WinRate from "../components/profile/winRate";
 
 import "../static/css/profile.css";
 
@@ -14,15 +15,16 @@ const Profile = () => {
             <div className="row">
                 <div className="col-md-6 d-flex flex-column align-items-center">
                     <FotoUser/>
-
                     <GlobalPunc/>
-
                     <Achievements/>
-
                     <FriendRequest/>
                 </div>
 
                 <div className="col-md-6 d-flex flex-column align-items-center">
+                    <span className="stats-title">ESTADÍSTICAS DE JUGADOR</span>
+                    <div id="stats-container">
+                        <WinRate/>
+                    </div>
                 </div>
                 
             </div>
