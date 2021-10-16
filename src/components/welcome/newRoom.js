ebc7ac24fec1d715792ca4fb4ef71d621fdeadcb
Francisco Javier Cavero López <franciscojaviercaverolopez@MacBook-Pro-de-Javier.local>
2021-10-16 19:23:48 +0200

    Página de welcome hecha

---
+++ src/components/welcome/newRoom.js
@@ -0,0 +1,16 @@
+const NewRoom = () => {
+    return(
+
+        <>
+            <div className="row justify-content-center align-items-center class-pretty-margin class-publicRooms-title">
+                <strong>¡Crea tu propia sala!</strong>
+            </div>
+            <div className="row justify-content-center align-items-center class-pretty-margin2">
+                <button className="btn btn-primary class-newRoom-button">CREAR</button>
+            </div>
+        </>
+
+    );
+}
+
+export default NewRoom;
